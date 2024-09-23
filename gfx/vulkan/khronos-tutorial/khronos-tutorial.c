ba85105ddf5cc942532bf4a30c81179d5453ecb4
misomosi <misomosispi@gmail.com>
2024-09-23 09:28:14 -0400

    Switch to SDL2 and Visual Studio

--- gfx/vulkan/khronos-tutorial/khronos-tutorial.c
+++ gfx/vulkan/khronos-tutorial/khronos-tutorial.c
@@ -1,15 +1,50 @@
-#include <GLFW/glfw3.h>
+#include <SDL2/SDL.h>
+#include <vulkan/vulkan.h>
+#include <stdio.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 #define WIDTH 640
 #define HEIGHT 480
 
-int main()
+int main(int argc, char **argv)
 {
-	glfwInit();
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Khronos Tutorial", NULL, NULL);
-	while (!glfwWindowShouldClose(window)) {
-		glfwPollEvents();
+	SDL_SetMainReady();
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL not initialized! %s\n", SDL_GetError());
+		return 1;
 	}
+
+	SDL_Window *window = SDL_CreateWindow(
+		"Khronos Tutorial",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		WIDTH,
+		HEIGHT,
+		SDL_WINDOW_SHOWN | SDL_WINDOW_VULKAN);
+	if (window == NULL)
+	{
+		printf("Window could not be created! %s", SDL_GetError());
+		return 1;
+	}
+
+	SDL_Event e;
+	bool quit = false;
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			switch (e.type)
+			{
+			case SDL_QUIT:
+				quit = true;
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
--- windows.mak
+++
@@ -1,8 +0,0 @@
-BUILD=build
-VKT=gfx\vulkan\khronos-tutorial
-GLFW_LIBS=$(BUILD)\glfw3dll.lib
-VKT_SOURCES=$(VKT)\khronos-tutorial.c $(GLFW_LIBS)
-
-DEBUGFLAGS=/Zi /I ..\include
-$(BUILD)\khronos-tutorial.exe : $(VKT_SOURCES)
-	cd build & cl.exe $(patsubst %,..\\%, $**) $(DEBUGFLAGS) 
